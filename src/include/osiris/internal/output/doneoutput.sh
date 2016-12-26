5505cc08209d726893a2219355fae2ff09a391f7
matthias <matthias@calvin>
2016-12-26 18:53:42 +0100

    basics for output

--- src/include/osiris/internal/output/doneoutput.sh
+++ src/include/osiris/internal/output/doneoutput.sh
@@ -1,1 +1,15 @@
 #!/usr/bin/env bash
+_osiris_internal_output_doneoutput__done_image() {
+	printf "done output image\n"
+}
+
+_osiris_internal_output_doneoutput_main() {
+	case "${OUTPUT_TYPE}" in
+		device)
+			;;
+
+		image)
+			_osiris_internal_output_doneoutput__done_image
+			;;
+	esac
+}
--- src/include/osiris/internal/output/initoutput.sh
+++ src/include/osiris/internal/output/initoutput.sh
@@ -1,1 +1,29 @@
 #!/usr/bin/env bash
+_osiris_internal_output_initoutput__init_device() {
+	OUTPUT_DEVICE="$1"
+
+	printf "init output device\n"
+
+	OUTPUT_TYPE="device"
+}
+
+_osiris_internal_output_initoutput__init_image() {
+	OUTPUT_IMAGE="$1"
+
+	printf "init output image\n"
+
+	OUTPUT_TYPE="image"
+}
+
+_osiris_internal_output_initoutput_main() {
+	if [ -z "${OUTPUT_FILE}" ]; then
+		printf "fatal error: no output device or image file\n" >&2
+		exit 1
+	fi
+
+	if [ -b "${OUTPUT_FILE}" ]; then
+		_osiris_internal_output_initoutput__init_device "${OUTPUT_FILE}"
+	else
+		_osiris_internal_output_initoutput__init_image "${OUTPUT_FILE}"
+	fi
+}
