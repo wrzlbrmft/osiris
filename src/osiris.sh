269cc008c2a3eda5b2e4653cf76cb40ac22d07f1
matthias <matthias@calvin>
2016-12-26 16:39:24 +0100

    added SCRIPT vars

--- src/osiris.sh
+++ src/osiris.sh
@@ -1,2 +1,8 @@
 #!/usr/bin/env bash
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+SCRIPT_FILE="${SCRIPT_DIR}/$(basename "${BASH_SOURCE[0]}")"
+SCRIPT_NAME="$(SCRIPT_NAME="$(basename "${BASH_SOURCE[0]}")"; printf "%s" "${SCRIPT_NAME%.*}")"
+
+# main
+
 exit 0
