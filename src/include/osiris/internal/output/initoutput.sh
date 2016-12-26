830a203c629d1f264e7c1f0c4f2c613499f7ee63
matthias <matthias@calvin>
2016-12-26 20:49:48 +0100

    outputs are files

--- src/include/osiris/internal/output/initoutput.sh
+++ src/include/osiris/internal/output/initoutput.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 _osiris_internal_output_initoutput__init_device() {
-	OUTPUT_DEVICE="$1"
+	OUTPUT_DEVICE_FILE="$1"
 
-	if [ -z "${OUTPUT_DEVICE}" ]; then
-		OUTPUT_DEVICE="${OUTPUT_FILE}"
+	if [ -z "${OUTPUT_DEVICE_FILE}" ]; then
+		OUTPUT_DEVICE_FILE="${OUTPUT_FILE}"
 	fi
 
-	if [ -z "${OUTPUT_DEVICE}" ]; then
+	if [ -z "${OUTPUT_DEVICE_FILE}" ]; then
 		printf "fatal error: no output device file\n" >&2
 		exit 1
 	fi
 
-	if [ -n "$(findmnt -ln -o SOURCE | grep "^${OUTPUT_DEVICE}")" ]; then
-		printf "fatal error: output device is currently mounted ('%s')\n" "${OUTPUT_DEVICE}" >&2
+	if [ -n "$(findmnt -ln -o SOURCE | grep "^${OUTPUT_DEVICE_FILE}")" ]; then
+		printf "fatal error: output device is currently mounted ('%s')\n" "${OUTPUT_DEVICE_FILE}" >&2
 		exit 1
 	fi
 
@@ -22,24 +22,24 @@ _osiris_internal_output_initoutput__init_device() {
 }
 
 _osiris_internal_output_initoutput__init_image() {
-	OUTPUT_IMAGE="$1"
+	OUTPUT_IMAGE_FILE="$1"
 
-	if [ -z "${OUTPUT_IMAGE}" ]; then
-		OUTPUT_IMAGE="${OUTPUT_FILE}"
+	if [ -z "${OUTPUT_IMAGE_FILE}" ]; then
+		OUTPUT_IMAGE_FILE="${OUTPUT_FILE}"
 	fi
 
-	if [ -z "${OUTPUT_IMAGE}" ]; then
+	if [ -z "${OUTPUT_IMAGE_FILE}" ]; then
 		printf "fatal error: no output image file\n" >&2
 		exit 1
 	fi
 
-	if [ -f "${OUTPUT_IMAGE}" ]; then
-		printf "fatal error: output image file already exists ('%s')\n" "${OUTPUT_IMAGE}" >&2
+	if [ -f "${OUTPUT_IMAGE_FILE}" ]; then
+		printf "fatal error: output image file already exists ('%s')\n" "${OUTPUT_IMAGE_FILE}" >&2
 		exit 1
 	fi
 
-	if [ ! -d "$(dirname "${OUTPUT_IMAGE}")" ]; then
-		mkdir -p "$(dirname "${OUTPUT_IMAGE}")"
+	if [ ! -d "$(dirname "${OUTPUT_IMAGE_FILE}")" ]; then
+		mkdir -p "$(dirname "${OUTPUT_IMAGE_FILE}")"
 	fi
 
 	printf "init output image\n"
