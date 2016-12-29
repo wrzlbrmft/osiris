2d74ed65b65774d5c0a477fc1f8821bdf874d2dc
matthias <matthias@calvin>
2016-12-29 14:50:43 +0100

    create dir for temp dir/file

--- src/include/osiris/utils/temp/include.lib.sh
+++ src/include/osiris/utils/temp/include.lib.sh
@@ -6,5 +6,19 @@ _osiris_utils_temp__create_temp_dir() {
 		TMPDIR="/tmp"
 	fi
 
+	_osiris_utils_fs__create_dir "${TMPDIR}"
+
 	TMPDIR="${TMPDIR}" mktemp -d
+}
+
+_osiris_utils_temp__create_temp_file() {
+	local TMPDIR="$1"
+
+	if [ -z "${TMPDIR}" ]; then
+		TMPDIR="/tmp"
+	fi
+
+	_osiris_utils_fs__create_dir "${TMPDIR}"
+
+	TMPDIR="${TMPDIR}" mktemp
 }
