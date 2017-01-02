67635af3c44684760320e69dcb37eaa0cc3914a1
matthias <matthias@calvin>
2017-01-02 20:31:19 +0100

    explicit vars declaration

--- src/include/osiris/utils/mount/include.lib.sh
+++ src/include/osiris/utils/mount/include.lib.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 __mount() {
-	local ID="$1"
-	local DEVICE_FILE="$2"
-	local MOUNT_DIR="$3"
+	local LABEL="$1"
+	local MOUNT_DIR="$2"
+	local DEVICE_FILE="$3"
 }
 
 __unmount() {
-	local ID="$1"
+	local LABEL="$1"
 }
 
 __unmount_all() {
